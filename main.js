3e83df9dc30734b6defdfe3ae3dd78322f233ca8
Yeetmaster7522 <121078943+Yeetmaster7522@users.noreply.github.com>
2026-03-06 18:54:21 +1100

    put navbar onto its on html file to make html easier to read and update

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+// Load navbar on each page
+fetch("navbar.html")
+    .then(res => res.text())
+    .then(html => {
+        document.getElementById("navbar").innerHTML = html;
+    });
+
+// load classes
 const jobFetcher = new JobPostFetcher();
