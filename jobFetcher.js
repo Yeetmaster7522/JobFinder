32813dad5e2abdb05b5190d17708da9ed662fafb
Yeetmaster7522 <121078943+Yeetmaster7522@users.noreply.github.com>
2026-03-06 18:47:07 +1100

    hopefully fixed job post class

--- jobFetcher.js
+++ jobFetcher.js
@@ -1,10 +1,11 @@
 class JobPostFetcher {
     constructor() {
+        let response = "";
         fetch("/data/data-mR36NBv3VwjMRsFCY26z5.json")
             .then(response = response.json())
             .then(data => {
                 console.log(data);
             })
-            .catch(err => console.error("Error loading JSON:", err))
+            .catch(err => console.error("Error loading JSON:", err));
     }
 }
