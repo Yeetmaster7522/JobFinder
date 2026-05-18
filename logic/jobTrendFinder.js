1cf5fd5829f75c423ac0747f122e5590ea6bf195
Yeetmaster7522 <david.f.santillan@gmail.com>
2026-05-18 12:03:45 +1000

    made trending industries and skills shortages actually show something from the data

--- logic/jobTrendFinder.js
+++ logic/jobTrendFinder.js
@@ -19,12 +19,23 @@ class JobTrendFinder {
         return Array.from(companies).slice(0,10);
     }
 
-    // getTrendingIndustries() {
-    //     let industries = new Set()
-    //     for (let i=0; i<this.data.length; i++) {
+    getTrendingIndustries() {
+        let industries = new Map()
+        for (let i=0; i<this.data.length; i++) {
+            let ind = this.data[i].industry;
+            if (industries.has(ind)) {
+                industries.set(ind, industries.get(ind) + 1);
+            }
+            else {
+                industries.set(ind, 1);
+            }
+        }
 
-    //     }
-    // }
+        return [...industries.entries()]
+            .sort((a,b) => b[1] - a[1])
+            .slice(0,10)
+            .map(entry => entry[0]);
+    }
 
     getTrendRoles() {
         let roles = new Map();
@@ -44,22 +55,55 @@ class JobTrendFinder {
             .map(entry => entry[0]);
     }
 
+    getSkillsShortage() {
+        let skills = new Map();
+
+        for (let i=0; i<this.data.length; i++) {
+            for (const s of this.data[i].skills) {
+                if (skills.has(s)) {
+                    skills.set(s, skills.get(s) + 1);
+                }
+                else {
+                    skills.set(s, 1);
+                }
+            }
+        }
+
+        return [...skills.entries()]
+            .sort((a,b) => b[1] - a[1])
+            .slice(0,10)
+            .map(entry => entry[0]);
+
+    }
+
+    appendLI(parent, childText) {
+        let li = document.createElement("li");
+        li.textContent = childText;
+        parent.appendChild(li);
+    }
+
     displayResults() {
         const nearEmployers = document.getElementById("near-employers");
+        const trendIndustries = document.getElementById("trend-industries");
         const trendRoles = document.getElementById("trend-roles");
+        const skillShortage = document.getElementById("skill-shortages");
 
         const employers = this.getNearEmployers();
+        const industries = this.getTrendingIndustries();
         const roles = this.getTrendRoles();
+        const skills = this.getSkillsShortage();
 
         for (let i=0; i<employers.length; i++) {
-            let li = document.createElement("li");
-            li.textContent = employers[i];
-            nearEmployers.appendChild(li);
+            this.appendLI(nearEmployers, employers[i]);
+        }
+        for (let i=0; i<industries.length; i++) {
+            this.appendLI(trendIndustries, industries[i]);
         }
         for (let i=0; i<roles.length; i++) {
-            let li = document.createElement("li");
-            li.textContent = roles[i];
-            trendRoles.appendChild(li);
+            this.appendLI(trendRoles, roles[i]);
+        }
+        for (let i=0; i<skills.length; i++) {
+            this.appendLI(skillShortage, skills[i]);
         }
     }
 }
