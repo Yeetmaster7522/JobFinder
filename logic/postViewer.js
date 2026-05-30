f152b92f05a48d677f2669fa242cd4b33d063789
Yeetmaster7522 <121078943+Yeetmaster7522@users.noreply.github.com>
2026-05-30 11:15:51 +0000

    made stuff show and disabled field inputs that shouldnt be changed

--- logic/postViewer.js
+++ logic/postViewer.js
@@ -19,8 +19,22 @@ class PostViwer {
 
         return foundPost;
     }
+
+    showPostDetails() {
+        document.getElementById("job-title-entry").value = this.post.jobTitle;
+        document.getElementById("work-type-entry").value = this.post.workType;
+        document.getElementById("employment-type-entry").value = this.post.employmentType;
+        document.getElementById("industry-entry").value = this.post.industry;
+        document.getElementById("hours-entry").value = "";
+        document.getElementById("skills-entry").value = "";
+        document.getElementById("description-entry").value = this.post.summary;
+        document.getElementById("salary-min-entry").value = this.post.salaryMin;
+        document.getElementById("salary-max-entry").value = this.post.salaryMax;
+        document.getElementById("address-entry").value = this.post.address;
+    }
 }
 
 window.addEventListener("mainReady", () => {
     const viewer = new PostViwer(window.main.jobPosts);
+    viewer.showPostDetails();
 })
