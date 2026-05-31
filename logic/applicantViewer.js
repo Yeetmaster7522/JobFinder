e535b7ac3634b2fb9c6002b555fc22ecaf930491
Yeetmaster7522 <121078943+Yeetmaster7522@users.noreply.github.com>
2026-05-31 12:04:22 +0000

    updated mock data used for applications and created base code for viewing the applications

--- logic/applicantViewer.js
+++ logic/applicantViewer.js
@@ -1,9 +1,89 @@
 class ApplicantViewer {
-    constructor(applicants) {
-        console.log(applicants);
+    constructor(users, applications) {
+        this.users = users;
+        this.applications = [];
+
+        const params = new URLSearchParams(window.location.search);
+        const id = params.get("id");
+
+        for (const application of applications) {
+            if (application.JPID == id) {
+                this.applications.push(application);
+            }
+        }
+    }
+
+    showAll() {
+        for (const application of this.applications) {
+            this.createPreview();
+        }
+    }
+
+    createPreview(userId) {
+        const previews = document.getElementById("previews");
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+        
+        const row1 = document.createElement("div");
+        row1.classList.add("row");
+        const col1 = document.createElement("div");
+        col1.classList.add("col");
+        const name = document.createElement("p");
+        name.textContent = "applicant name";
+
+        const row2 = document.createElement("div");
+        row2.classList.add("row");
+        const col2 = document.createElement("div");
+        col2.classList.add("col");
+        const email = document.createElement("p");
+        email.textContent = "email";
+
+        const row3 = document.createElement("div");
+        row3.classList.add("row");
+        const col3 = document.createElement("div");
+        col3.classList.add("col");
+        const number = document.createElement("p");
+        number.textContent = "number";
+
+        const row4 = document.createElement("div");
+        row4.classList.add("row");
+        const col4_1 = document.createElement("div");
+        col4_1.classList.add("col");
+        const applied = document.createElement("p");
+        applied.textContent = "Applied:";
+        const col4_2 = document.createElement("div");
+        col4_2.classList.add("col");
+        const date = document.createElement("p");
+        date.textContent = "DD.MM";
+
+        col1.appendChild(name);
+        row1.appendChild(col1);
+
+        col2.appendChild(email);
+        row2.appendChild(col2);
+
+        col3.appendChild(number);
+        row3.appendChild(col3);
+
+        col4_1.appendChild(applied);
+        col4_2.appendChild(date);
+        row4.appendChild(col4_1);
+        row4.appendChild(col4_2);
+
+        li.appendChild(row1);
+        li.appendChild(row2);
+        li.appendChild(row3);
+        li.appendChild(row4);
+        
+        previews.append(li);
+    }
+
+    showDetails() {
+
     }
 }
 
 window.addEventListener("mainReady", () => {
-    const applicantViewer = new ApplicantViewer(window.main.applications);
+    const applicantViewer = new ApplicantViewer(window.main.users, window.main.applications);
+    applicantViewer.showAll();
 });
